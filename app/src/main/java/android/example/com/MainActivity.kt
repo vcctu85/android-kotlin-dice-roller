a91d024a68cd91e7b603bb3374483040be43d495
Vicky Tu <vcctu85@berkeley.edu>
2018-06-05 11:41:18 -0700

    made revisions

--- app/src/main/java/android/example/com/MainActivity.kt
+++ app/src/main/java/android/example/com/MainActivity.kt
@@ -1,33 +1,34 @@
 package android.example.com
 
-import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val DICE_SIDE_NUMBER = 6
+        const val DRAWABLE = "drawable"
+        const val DICE_NAME = "ic_dice"
+    }
+
+    private val random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById(R.id.button) as Button
         button.setOnClickListener {
-            //find imageView
-            val image: ImageView = findViewById(R.id.imageView)
-            randomGenerator(image)
+            rollDice()
         }
     }
 
-    fun randomGenerator(image: ImageView) {
-        val random = Random()
-        val randomInt: Int = random.nextInt(6) + 1
-        val img_name: String = "ic_dice" + randomInt
+    private fun rollDice() {
+        val randomInt = random.nextInt(DICE_SIDE_NUMBER) + 1
+        val imgName = DICE_NAME + "$randomInt"
 
-        val resources: Resources = getResources()
-        val identifier: Int = resources.getIdentifier(img_name, "drawable", getPackageName())
-        image.setImageResource(identifier)
+        val identifier = resources.getIdentifier(imgName, DRAWABLE, packageName)
+        imageView.setImageResource(identifier)
     }
 }
